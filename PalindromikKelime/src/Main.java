2a2284d1323b3e2d32677bbeca1e82db9b4f4ca0
ahmettolay <101968628+ahmettolay@users.noreply.github.com>
2022-08-31 22:16:22 +0300

    Add files via upload

---
+++ PalindromikKelime/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    static boolean isPalidrom(String str) {
+
+        int i = 0;
+        int j = str.length() - 1;
+
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    static boolean isPalidrom2(String str) {//kayak
+
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+
+            reverse += str.charAt(i);
+
+        }
+        return str.equals(reverse);
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(isPalidrom2("kayak"));
+
+    }
+}
